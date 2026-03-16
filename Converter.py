ff4a62b766021cc2a5e9e3be71c3f660494799ae
lucasfreitas0 <lucasyurisoutodefreitas@gmail.com>
2026-03-16 01:36:33 -0300

    erro de update fixed

--- Converter.py
+++ Converter.py
@@ -5,10 +5,8 @@ tabela_conversao = {}
 
 try:
     with open(caminho_arquivo, 'r', encoding='utf-8') as f:
-        # 1. Carregamos o arquivo
         conteudo = json.load(f)
         
-        # 2. Se o seu JSON for uma lista de um único dicionário:
         if isinstance(conteudo, list) and len(conteudo) > 0:
             tabela_conversao = conteudo[0]
         else:
@@ -24,12 +22,10 @@ except json.JSONDecodeError:
 simbolo_procurado = " "
 encontrado = False
 
-# 3. Agora tabela_conversao tem os dados reais
 for id_chave, valores in tabela_conversao.items():
     if id_chave == "_comentario":
         continue
     
-    # Verifique se 'valores' é realmente um dicionário antes de acessar ["simbolo"]
     if isinstance(valores, dict) and valores.get("simbolo") == simbolo_procurado:
         print(f"Símbolo '{simbolo_procurado}' achado no ID: {id_chave}")
         print(f"Detalhes: {valores}")
